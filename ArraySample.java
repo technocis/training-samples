590bad91b3ffc2d4430f8790ed3b3ad7f13eb654
manojjohn1838 <manojjohn1838@gmail.com>
2022-02-17 17:26:40 +0530

    arraysample file added

---
+++ ArraySample.java
@@ -0,0 +1,18 @@
+package com.technosys.datatypes;
+
+public class ArraySample {
+	public static void main(String[] args) {
+		//Array declaration
+		int [] intArray=new int[10];
+		
+		//initialized array
+		int [] intArray1={1,2,34,5};
+		
+		System.out.println(intArray[0]); //random access
+		
+		
+		
+		
+		
+	}
+}
